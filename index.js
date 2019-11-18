08c3eeace2a8e7256b0342e04b42867b2e56a7fc
github username <github email address>
2019-11-18 06:29:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+function findMatching(array, string) {
+   return array.filter(name => name.toLowerCase() == string.toLowerCase())
+}
+
+
+function fuzzyMatch(array, string) {
+   return array.filter(name => name.startsWith(string))
+}
+
+function matchName(array, string) {
+   return array.filter(object => object.name === string)
+}
