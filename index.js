6f87327a708fbfc6cd171a0de5a640ef7c733f36
lanibon <nguyenlan13@gmail.com>
2019-11-18 22:04:15 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(driver, string){
+    return driver.filter(match => match.toUpperCase() === string.toUpperCase()
+)
+}
+
+function fuzzyMatch(driver, string){
+    return driver.filter(fuzzy => fuzzy.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+}
+
+function matchName(driver, string){
+    return driver.filter(match => match.name === string)
+}
