53cb493fb595579c74f4afc2e63eb68d5ab08094
Dan Sullivan <dansullivan19@gmail.com>
2019-11-21 08:50:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+// // Code your solution here
+
+// function findMatching(drivers, string){
+//     let matches = drivers.filter(function(d) { return d === `${string}`});
+//     return matches;
+// }
+
+
+// function fuzzyMatch() {
+
+// }
+
+
+// function matchName () {
+    
+// }
+
+function findMatching(array, string) {
+    return array.filter( d => d.toLowerCase() === string.toLowerCase() )
+  }
+  
+  function fuzzyMatch(array, string) {
+    return array.filter( d => d.startsWith(string) )
+  }
+  
+  
+  
+  function matchName(array, string) {
+    return array.filter( d => d['name'] === string )
+  }
