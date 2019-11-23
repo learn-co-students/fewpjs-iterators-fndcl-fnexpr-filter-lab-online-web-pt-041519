8f133f99f9a3c08eaa8b1a464c205097ab3fc547
blackwellsmith <blackwellsmithiv@gmail.com>
2019-11-23 16:50:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution here
+//words.filter(word => word.length > 6);
+function fuzzyMatch(array, string) {
+   return array.filter(driver => driver.toUpperCase().indexOf(string.toUpperCase()) === 0)
+}
