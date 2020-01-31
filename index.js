4e80714edf7c58a304a463dc7a9c91d87c35b7cc
PineappleGuy <garff247@gmail.com>
2020-01-31 13:22:55 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(e => {
+        return e === string || e === string.toLowerCase();
+    });
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(e => {
+        return e.charAt(0) === string.charAt(0)
+    })
+}
+
+function matchName(array, string) {
+    return array.filter(e => {
+        return e.name === string
+    })
+}
